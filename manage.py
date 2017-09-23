9298a15da0e461024308e094b96f0a6e0ada66b8
wyt <353981086@qq.com>
2017-09-23 11:03:33 +0800

    添加Tag，Commit模型

--- manage.py
+++ manage.py
@@ -1,14 +1,18 @@
 from flask_script import Manager, Server
-from main import app, db, User, Post
+from flask_migrate import Migrate, MigrateCommand
+from main import app, db, User, Post, Tag, Comment
 
+
+migrate = Migrate(app, db)
 
 manager = Manager(app)
 manager.add_command("server", Server())
+manager.add_command("db", MigrateCommand)
 
 
 @manager.shell
 def make_shell_context():
-    return dict(app=app, db=db, User=User, Post=Post)
+    return dict(app=app, db=db, User=User, Post=Post, Tag=Tag, Comment=Comment)
 
 
 if __name__ == '__main__':
